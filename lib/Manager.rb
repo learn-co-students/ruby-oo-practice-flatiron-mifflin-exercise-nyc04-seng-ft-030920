51959ab865609b00a629b189e693c304dc4fcf9d
replacem3nts <donny.landis@gmail.com>
2020-03-14 22:20:02 -0400

    updates files to meet all deliverables

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,35 @@
 class Manager
+attr_accessor :dept, :age
+attr_reader :name
+    @@all = []
 
+    def initialize(name, dept, age)
+        @name = name
+        @dept = dept
+        @age = age
+        @@all << self
+    end
+
+    def employees
+        Employee.all.select {|employee| employee.manager == self}
+    end
+
+    def hire_employee(new_emp, salary)
+        new_hire = Employee.all.detect {|employee| employee.name == new_emp && employee.salary == salary}
+        new_hire.manager = self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+        d = self.all.map {|manager| manager.dept}
+        d.uniq
+    end
+
+    def self.average_age
+        a = self.all.map {|manager| manager.age}
+        a.sum / a.count
+    end
 end
