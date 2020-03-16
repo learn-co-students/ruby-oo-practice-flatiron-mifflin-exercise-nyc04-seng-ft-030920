275f24b515fa8d3a670120503594c748f354099f
isabelxklee <hello@isabelklee.co>
2020-03-15 22:08:17 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,47 @@
 class Employee
 
-end
+    attr_accessor :name, :salary, :manager
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary.to_i
+        @manager = manager
+        @@all << self
+    end 
+
+    def self.paid_over(num)
+        big_salaries = Employee.all.select { |employee|
+            employee.salary > num
+        }
+        big_salaries
+    end 
+
+    def self.find_by_department(department)
+        # take a department
+        # check to see if there are any managers working in that department
+        # return the first employee that matches that manager
+
+        manager = Manager.all.find { |manager|
+            manager.department == department
+        }
+
+        manager.employees[0]
+    end
+
+    def tax_bracket
+        employees = Employee.all.select { |employee|
+            employee.salary < self.salary + 1000 && employee.salary > self.salary - 1000
+        }
+        employees
+    end
+
+    def self.all 
+        @@all
+    end 
+
+    def inspect 
+        "<Employee name: #{name}, Salary: #{salary}, Manager: #{manager}>"
+    end 
+
+end 
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,49 @@
-class Manager
+class Manager 
+    attr_accessor :name, :department, :age
+    @@all = []
 
-end
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age.to_i
+        @@all << self
+    end 
+
+    def employees
+        managers_employees = Employee.all.select { |employee|
+            employee.manager == self
+        }
+        managers_employees
+    end 
+
+    def hire_employee(employee_name, salary)
+        Employee.new(employee_name, salary, self)
+    end 
+
+    def self.all_departments 
+        departments = []
+        Manager.all.each { |manager|
+             departments << manager.department
+        }
+
+        departments
+    end 
+
+    def self.average_age
+        avg_ages = Manager.all.map { |manager|
+            manager.age
+        }
+
+        avg_ages = avg_ages.sum.to_f / avg_ages.length
+        avg_ages
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def inspect 
+        "<Manager name: #{name}, Age: #{age}, Department: #{department}>"
+    end 
+
+end 
